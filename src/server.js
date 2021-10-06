9e2a654b3822eb65ef49ae5b7f097553a2502241
Mardon <tursunovmardon7@gmail.com>
2021-10-06 18:10:20 +0500

    updated files

--- src/server.js
+++ src/server.js
@@ -3,3 +3,20 @@ const path = require('path')
 const PORT = process.env.PORT || 3000
 
 const app = express()
+
+
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('sequelize', 'postgres', 'mardon', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+sequelize.authenticate()
+    .then(() => console.log("Database has connected successfuly!"))
+    .catch((error) => console.log(error))
+
+
+app.get('/', (req, res) => res.send("Index"))
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
